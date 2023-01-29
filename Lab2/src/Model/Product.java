19eb40fc9136d99d299fca9a7755d8b58a176ce8
Alan <yao.han1@northeastern.edu>
2023-01-29 16:08:29 -0500

    lab2

---
+++ Lab2/src/Model/Product.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author alan
+ */
+public class Product {
+    int productId;
+    double price;
+    String productName;
+    
+    public Product() {
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    
+}
